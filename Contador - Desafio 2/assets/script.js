a42b8cda0d32732de59e36cbead9807b06b7010b
jessicacfsb <jessica.cfsb@gmail.com>
2021-12-30 18:01:24 -0300

    Desafio contador - DIO

--- Contador - Desafio 2/assets/script.js
+++ Contador - Desafio 2/assets/script.js
@@ -1,12 +1,18 @@
 var numeroInicial = document.getElementById("currentNumber")
 var numeroAtual = 0
 
-function increment () {
-    numeroAtual = numeroAtual + 1
-    numeroInicial.innerHTML = numeroAtual
-}
+    function increment () {
+        numeroAtual ++
+        numeroInicial.innerHTML = numeroAtual
+        if (numeroAtual >= 0 && numeroAtual > 10){  
+            window.alert("ERRO... Contador Acima do limite")
+        }
+    }
 
-function decrement () {
-    numeroAtual = numeroAtual - 1
-    numeroInicial.innerHTML = numeroAtual
+    function decrement () {
+        numeroAtual --
+        numeroInicial.innerHTML = numeroAtual
+        if (numeroAtual < 0 && numeroAtual < -10){
+            window.alert("ERRO... Contador Abaixo do limite")
+        }
 }

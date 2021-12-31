94a73e324126f1bb8c53f2adddea94b891b0dbb0
jessicacfsb <jessica.cfsb@gmail.com>
2021-12-31 19:20:24 -0300

    desafio to-do List

---
+++ To-do list - Desafio 3/assets/script.js
@@ -0,0 +1,9 @@
+var input = document.getElementById("task-input")
+var clicar = document.getElementById("submeter")
+var atividades = document.getElementById("tasks")
+
+function adicionar(){
+    if (input.length > 0){
+        atividades = document.createElement("li")
+    }
+}

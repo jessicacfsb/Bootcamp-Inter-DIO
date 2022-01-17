8a802ff8739bcbea1364a2a98c0e8fabf93039d4
jessicacfsb <jessica.cfsb@gmail.com>
2022-01-17 18:54:59 -0300

    Desafio 7 - Funções e o uso do this

---
+++ Desafio 7 - Funções/Atividade 1 - Alunos Aprovados/script.js
@@ -0,0 +1,35 @@
+/*const array = [7.0, 5, 4, 10]
+
+function alunos(mediaFinal){
+    for (let i = 0; i < mediaFinal.length; i++){
+        if (mediaFinal[i] < 7){
+            mediaFinal[i] = mediaFinal.splice()
+        }
+    }
+    return mediaFinal
+}
+console.log(alunos(array))*/
+
+const alunos = [
+	{
+		nome: 'João',
+		nota: 5,
+		turma: '1B',
+	},
+	{
+		nome: 'Sofia',
+		nota: 9,
+		turma: '1B',
+	},
+	{
+		nome: 'Paulo',
+		nota: 6,
+		turma: '2C',
+	},
+]
+
+function alunosAprovados (array, media){
+    for (let i = 0; i < mediaFinal.length; i++){
+        
+    }
+}
